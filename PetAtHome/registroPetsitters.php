204b6a1f59ccdf72bd4b27927960a1e3588c1d95
Nicolás Andres  Leal González <nicolasandres.mecatronica@gmail.com>
2022-06-21 16:56:50 -0500

    Database connected

---
+++ PetAtHome/registroPetsitters.php
@@ -0,0 +1,43 @@
+<?php
+ini_set('display_errors', 1);
+
+error_reporting(E_ALL);
+
+
+include('db-registroPetsitters.php');
+
+$nombres = $_REQUEST['txtNombre'];
+$apodo = $_REQUEST['txtApodo'];
+$correo = $_REQUEST['txtCorreo'];
+$pais = $_REQUEST['txtPais'];
+$ciudad = $_REQUEST['txtCiudad'];
+$edad = $_REQUEST['nEdad'];
+$cell = $_REQUEST['nCell'];
+$address = $_REQUEST['txtDireccion'];
+$usuario = $_REQUEST['txtUsuario'];
+$password = $_REQUEST['txtPassword'];
+
+
+$consulta = "INSERT INTO petsitters (`Nombre Completo`, `Apodo`, `Correo`, `Pais`, `Ciudad`, `Edad`, `Telefono`, `Direccion`, `Usuario`, `Password`) VALUES ('$nombre', '$apodo', '$correo', '$pais', `$ciudad`, `$edad`, `$cell`, `$address`, '$usuario', '$password');";
+
+$resultado = mysqli_query($conexion, $consulta) or die("error de registro");
+
+
+echo "registro exitoso";
+
+if ($resultado) {
+    header("location:index.html");
+} else {
+    include("cuidadores.html");
+    ?>
+    <h1>ERROR DE REGISTRO</h1>
+    <?php
+}
+
+
+
+mysqli_close($conexion);
+
+
+
+?>
--- PetAtHome/registroUsers.php
+++ PetAtHome/registroUsers.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 
-include('db-registro.php');
+include('db-registroUsers.php');
 
 $nombre = $_REQUEST['txtNombre'];
 $apellido = $_REQUEST['txtApellido'];
